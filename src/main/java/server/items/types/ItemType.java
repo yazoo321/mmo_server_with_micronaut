8cbad456739a53fc699ae8e79650c81c948ec70a
Yaroslav Lazarev <y.lazarev@hotmail.com>
2022-06-27 08:47:03 +0100

    Remove unneeded comment

--- src/main/java/server/items/types/ItemType.java
+++ src/main/java/server/items/types/ItemType.java
@@ -3,19 +3,6 @@ package server.items.types;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-//@Getter
-//public enum ItemType {
-//    ARMOUR("ARMOUR"),
-//    WEAPON("WEAPON"),
-//    CONSUMABLE("CONSUMABLE");
-//
-//    public final String type;
-//
-//    private ItemType(String type) {
-//        this.type = type;
-//    }
-//
-//}
 
 @Getter
 @AllArgsConstructor
